aa4618b93a773c1dc8d534a5f09464e7461f151d
DogukanUcak <dogukanucak94@gmail.com>
2019-12-19 10:29:37 +0300

    Add props to justify container

--- src/components/UIElements/date/DateFull.js
+++ src/components/UIElements/date/DateFull.js
@@ -1,44 +1,53 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import { Typography, Box } from '@material-ui/core';
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import { makeStyles } from '@material-ui/styles';
-
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Typography, Box } from "@material-ui/core";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { makeStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
 const useStyles = makeStyles(theme => ({
-    mainContainer :{
-        display:'flex',
-        justifyContent:'flex-start',
-        
-    },
-    center: {
-        fontSize: "16px",
-        alignSelf: 'center',
-        [theme.breakpoints.down("md")]: {
-            fontSize: "12px"
-        }
-    },
-    dateText:{
-        fontSize:"14px",
-        [theme.breakpoints.up("sm")]: {
-            fontSize: "16px"
-        },
+  mainContainer: {
+    display: "flex",
+    justifyContent: props => props.justifyContent
+  },
+  center: {
+    fontSize: "16px",
+    alignSelf: "center",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "12px"
     }
+  },
+  dateText: {
+    fontSize: "14px",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "16px"
+    }
+  }
 }));
 
-// Stringleri objeye çevir
-
 export default function DateFull(props) {
-    const classes = useStyles();
+  const classes = useStyles(props);
 
-    return (
-        <Grid container className={classes.mainContainer} direction="row" wrap="nowrap">
-            <Box mr={1}>
-                <Typography className={classes.dateText}>{props.startDate}</Typography>
-            </Box>
-            <ArrowForwardIosIcon className={classes.center} size="medium"  />
-            <Box  ml={1}>
-                <Typography className={classes.dateText}>{props.endDate}</Typography>
-            </Box>
-        </Grid>
-    )
+  return (
+    <Grid
+      container
+      className={classes.mainContainer}
+      direction="row"
+      wrap="nowrap"
+    >
+      <Box mr={1}>
+        <Typography className={classes.dateText}>{props.startDate}</Typography>
+      </Box>
+      <ArrowForwardIosIcon className={classes.center} size="medium" />
+      <Box ml={1}>
+        <Typography className={classes.dateText}>{props.endDate}</Typography>
+      </Box>
+    </Grid>
+  );
 }
+
+DateFull.propTypes = {
+  justifyContent: PropTypes.string
+};
+DateFull.defaultProps = {
+  justifyContent: "flex-start"
+};
