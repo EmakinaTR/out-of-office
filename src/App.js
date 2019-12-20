acf65cbee5137a066bdb9127ef65c21cd1b5f9c6
DogukanUcak <dogukanucak94@gmail.com>
2019-12-20 15:37:04 +0300

    Add Responsive Spacing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import Navigation from "./components/UIElements/navigation";
 import AuthContext from "./components/session";
 import { firebaseConfig } from "./components/firebase/config";
-
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 function App() {
   const [isLoggedIn, setLoggedIn] = useState(true);
   const breakpointValues = {
@@ -13,23 +13,31 @@ function App() {
     sm: 576,
     md: 768,
     lg: 992,
-    xl: 1200,
+    xl: 1200
   };
-  const theme = createMuiTheme({
+  let theme = createMuiTheme({
     palette: {
       primary: {
         light: "#757ce8",
         main: "#000000",
         dark: "#000000",
         contrastText: "#fff",
+        __TEST__: "#aaaaaa"
       },
       secondary: {
         main: "#e0e0e0",
-        dark: "#e0e0e0",
+        dark: "#e0e0e0"
       }
     },
-    breakpoints: {values:breakpointValues}
+    breakpoints: { values: breakpointValues }
   });
+
+  const isLarge = useMediaQuery(theme.breakpoints.up("md"));
+  const _spacing = theme.spacing;
+  theme.spacing = value => {
+    value = isLarge ? value : value * 0.6;
+    return _spacing(value);
+  };
   function readSession() {
     const user = window.sessionStorage.getItem(
       `firebase:authUser:${firebaseConfig.apiKey}:[DEFAULT]`
