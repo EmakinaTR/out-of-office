aa0e8eac7fbe1f3d7adda0c73480e8d51791cac8
Özhan Mavili <o.mavili@emakina.com.tr>
2019-12-24 10:41:05 +0300

    list controls responsive

--- src/components/UIElements/searchFilter/SearchFilter.js
+++ src/components/UIElements/searchFilter/SearchFilter.js
@@ -1,94 +1,32 @@
 import React, { useState } from 'react';
-import { InputBase,Input,Box }from '@material-ui/core';
+import { InputBase,Input,InputAdornment,Box }from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import {  makeStyles } from '@material-ui/core/styles';
 const SearchFilter = ({onChange})=> {
-    const useStyles = makeStyles(theme => ({
-        search: {
-           display: 'flex',
-           flexDirection: 'row',
-           flex: '1 0',
-           alignItems: 'center'
-        }
-        /* root: {
-            flexGrow: 1,
-        },
-        search: {
-            position: 'relative',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: "white",
-            '&:hover': {
-                backgroundColor: (theme.palette.common.white, 0.25),
-            },
-            marginLeft: 0,
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(1),
-                width: 'auto',
-            },
-        },
-        searchIcon: {
-            width: theme.spacing(7),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        inputRoot: {
-            color: 'inherit',
-            
-        },
-        inputInput: {
-            overflow: 'hidden',
-            padding: theme.spacing(1, 1, 1, 7),
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            // [theme.breakpoints.up('sm')]: {
-            //     width: 120,
-            //     '&:focus': {
-            //         width: 200,
-            //     },
-            // },
-        }, */
-    }));
+
     const [searchQuery,setSearchQuery]=useState('');
-    const classes = useStyles();
+
    
     const onChangeHandler =(e)=>{
         setSearchQuery(e.target.value);
         onChange(e.target.value);
     }
     return (
-        <div className={classes.search}>
-            <SearchIcon />
+        <div>
             <Input
                 placeholder="Search…"
                 type="text"
                 value={searchQuery}
                 onChange={onChangeHandler}
+                startAdornment={
+                    <InputAdornment position="start">
+                      <SearchIcon />
+                    </InputAdornment>
+                  }
                 inputProps={{ 'aria-label': 'search' }}
+                style={{width:"100%", maxWidth:"16rem"}}
             />
         </div>
-/*         <div className={classes.search}>
-            <div className={classes.searchIcon}>
-                
-            </div>
-            <SearchIcon />
-            <Input
-                placeholder="Search…"
-                
-                type="text"
-                value={searchQuery}
-                onChange={onChangeHandler}
-                classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-            />
-        </div> */
     );
 }
 export default SearchFilter;
--- src/pages/myrequests/MyRequests.js
+++ src/pages/myrequests/MyRequests.js
@@ -10,12 +10,24 @@ import FilterListIcon from '@material-ui/icons/FilterList';
 import { FilterBox } from '../../components/UIElements/filterBox/FilterBox';
 
 const useStyles = makeStyles(theme => ({
-
     contentContainer: {
-        padding: "0",
+        padding:0
     },
     headerContainer: {
         //margin: theme.spacing(2)
+    },
+    listControls: {
+        '& :first-child': {
+            flexGrow: 1,
+            [theme.breakpoints.up('lg')]: {
+                flexGrow: 0
+            },
+         },
+        justifyContent: "flex-start",
+        [theme.breakpoints.up('lg')]: {
+            justifyContent: "flex-end",
+        },
+        
     }
 }));
 const orderByFilterOptions = {
@@ -81,17 +93,21 @@ export default function MyRequests(props) {
     return (
         <Container className={classes.contentContainer}  >
 
-            <Box >
-                <Grid container className={classes.headerContainer}>
+            <Box marginBottom={2}>
+                <Grid container className={classes.headerContainer}  alignItems="center">
                     <Grid item xs={12} lg={4}>
                     <Typography variant="h5" component="h2">My Requests</Typography>
                       
                     </Grid>
                     <Grid item xs={12} lg={8} >
+                        <Grid container alignItems="center" spacing={2} className={classes.listControls} wrap="nowrap">
+                        <Grid item>
                         <SearchFilter
                             onChange={onSearchQueryChange}
                         >
                         </SearchFilter>
+                        </Grid>
+                        <Grid item>
                         <OrderByFilter
                             options={orderByFilterOptions}
                             onFilterDirectionChanged={onFilterDirectionChanged}
@@ -101,14 +117,17 @@ export default function MyRequests(props) {
                         >
 
                         </OrderByFilter>
+                        </Grid>
+                        <Grid item>
                         <FilterBox
                             onFilterBoxClick={onFilterBoxClick}
                             filterBoxState={filterBoxState}
                         >
-                        </FilterBox>
+                        </FilterBox></Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
-                
+                </Box>
                 {dataList.map((data, index) => {
                     // var statusType = statusBadges.find(type => type.id == data.status)
                     // var leaveType = leaveBadges.find(type => type.id == data.leaveType)
@@ -127,7 +146,7 @@ export default function MyRequests(props) {
                         ></MyRequestsCard>
                     )
                 })}
-            </Box>
+            
         </Container>
     )
 }
