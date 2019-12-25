4dcb3bb7acef1fb2dc6c682a7dba25029cfe3bde
ilkerunal <58402669+ilkerunal@users.noreply.github.com>
2019-12-25 13:50:01 +0300

    Uncomment request edit

--- src/pages/leaveRequestEdit/LeaveRequestEdit.js
+++ src/pages/leaveRequestEdit/LeaveRequestEdit.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import LeaveRequestEditForm from '../../components/UIElements/leaveRequestEditForm';
-import FilterBox from '../../components/UIElements/filterBox';
-import MoreDialog from '../../components/UIElements/moreDialog';
+
 
 export default function RequestDetail() {
     return (
-        // <LeaveRequestEditForm />
-        <MoreDialog></MoreDialog>
+        <LeaveRequestEditForm />
+        // <MoreDialog></MoreDialog>
     )
 }
