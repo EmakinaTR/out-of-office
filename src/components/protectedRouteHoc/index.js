932efbf975316d8920bb7aed4220e014137fe5c7
Ersoy Efe Uruk <e.uruk@emakina.com.tr>
2019-12-26 18:02:46 +0300

    LeaveRequestEditForm become dynamic

--- src/components/protectedRouteHoc/index.js
+++ src/components/protectedRouteHoc/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Redirect, withRouter, useHistory } from "react-router-dom";
 const ProtectedRouteHoc = ({ component: Component, isLoggedIn, ...rest }) => {
   const history = useHistory();
-  const currentPath = history.location.pathname;
+  const location = history.location;
   if (isLoggedIn || rest.public) {
     return (
       <Route
@@ -13,7 +13,7 @@ const ProtectedRouteHoc = ({ component: Component, isLoggedIn, ...rest }) => {
       />
     );
   }
-  return <Redirect to={{ pathname: currentPath }} />;
+  return <Redirect to={location} />;
 };
 
 export default withRouter(ProtectedRouteHoc);
