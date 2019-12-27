7127b0d326d5356f29713884da3c969989edc5dd
DogukanUcak <dogukanucak94@gmail.com>
2019-12-27 09:38:30 +0300

    Add getTeamLeave method

--- functions/index.js
+++ functions/index.js
@@ -5,14 +5,14 @@ admin.initializeApp();
 exports.onCreateUser = functions.auth.user().onCreate(async (userRecord, context) => {
     displayName = userRecord.displayName.split(' ')
     userDoc = {
-        email : userRecord.email,
+        email: userRecord.email,
         firstName: displayName[0],
         lastName: displayName[1],
         avatarURL: userRecord.photoURL || '',
         createdDate: admin.firestore.Timestamp.fromDate(new Date()),
-        isAdmin : false,
+        isAdmin: false,
         isApprover: false,
-        quitDate : ''
+        quitDate: ''
     }
     return admin.firestore().collection('users').doc(userRecord.uid)
         .set(userDoc)
@@ -23,11 +23,11 @@ exports.onCreateUser = functions.auth.user().onCreate(async (userRecord, context
         .catch(err => {
             console.log(err);
             return;
-    });
+        });
 });
 
 exports.onTeamLeadChange = functions.firestore.document('teams/{leadUser}')
-    .onUpdate( async (change, context) => {
+    .onUpdate(async (change, context) => {
         let newApproverID = change.after.get("leadUser");
         let retiredApproverID = change.before.get("leadUser");
         return admin.firestore().collection('users').doc(newApproverID).set({ isApprover: true }, { merge: true })
@@ -36,9 +36,9 @@ exports.onTeamLeadChange = functions.firestore.document('teams/{leadUser}')
                 admin.firestore().collection('users').doc(retiredApproverID).set({ isApprover: false }, { merge: true })
             })
             .catch(err => console.log(err))
-});
+    });
 
-exports.getMyRequests = functions.https.onCall(async (data, context) => { 
+exports.getMyRequests = functions.https.onCall(async (data, context) => {
     const userID = context.auth.uid;
     let leaveRequestArray = [];
     await admin.firestore().collection('leaveRequests').where("createdBy", "==", userID).get().
@@ -60,83 +60,106 @@ exports.getMyRequests = functions.https.onCall(async (data, context) => {
 });
 
 exports.getLeaveRequestDetail = functions.https.onCall(async (data, context) => {
-    const documentId = data.text; 
+    const documentId = data.text;
     await admin.firestore().doc('/leaveRequests/{documentId}').get()
-    .then( async querySnapshot => {
+        .then(async querySnapshot => {
             const leaveRequest = querySnapshot.data();
             await admin.firestore().doc(leaveRequest.leaveTypeRef.path).get()
-            .then(documentSnapshot => {
-                leaveRequest.leaveType = documentSnapshot.data();
-                
-            });
-            
+                .then(documentSnapshot => {
+                    leaveRequest.leaveType = documentSnapshot.data();
+
+                });
+
         })
-        .catch(err => { console.log(err);return ("Document not found") }
-    )
+        .catch(err => { console.log(err); return ("Document not found") }
+        )
     return leaveRequest;
 });
 
 
-exports.getTeamLeaves = functions.https.onCall(async (data, context) => {
-    let teamMemmbers = [];
-    let leaveRequestArray = [];
-    await admin.firestore().collection('teams').where('leadUser', "==", context.auth.uid).get()
-        .then(snapshot => {
-            if (!snapshot.empty) { // If he/she is, get members of the team
-                teamMemmbers = snapshot.docs[0].data().members;
-            } else {
-                return ("No data to display")
-            }
-
-        }).catch(err => console.log(err))
-    if (teamMemmbers.length > 0) {  // If the team has at least one memeber, retrieve their request data
-        await Promise.all(teamMemmbers.map(async (member) => {
-            console.log("member " + member)
-            await admin.firestore().collection('leaveRequests').where("createdBy", "==", member).get().
-                then(querySnapshot => {
-                 
-                    querySnapshot.docs.map(doc => {
-                        const docObject = doc.data();
-                        docObject.id = doc.id;
-                        leaveRequestArray.push(docObject);                      
-
-                    })
-                }).catch(err => console.log(err));
-        }))
+exports.getTeamLeavesC = functions.https.onCall(async (data, context) => {
+        const userId = context.auth.uid;
+        const user = await _getUser(userId);        
+        const leaveRequestArray = await _getLeaves(userId,_isAdmin(user));
+        console.log("LEAVE ARRAY BEFORE:", leaveRequestArray);
         if (leaveRequestArray.length > 0) {
             await Promise.all(leaveRequestArray.map(async (item) => { // Retrieve leave types of the leave requests
                 await admin.firestore().doc(item.leaveTypeRef.path).get().then(documentSnapshot => {
                     item.leaveType = documentSnapshot.data();
                 });
             }))
-        }
-
-    } else {
-        return ("Your team has no members")
-    }
-
+        }    
+        console.log("LEAVE ARRAY AFTER:", leaveRequestArray);
     return leaveRequestArray;
 });
 
+const _getLeaves = async (userid, isAdmin = false) => {
+    let leaves = []
+    if (isAdmin) {
+        leaves = _getAdminLeaves();
+    } else {
+        leaves = _getTeamLeaves(userid);
+    }    
+    return leaves;
+}
+
+const _getTeamLeaves = async (userid) => {
+    const leaveRequestArray = [];
+    let teamMemmbers = [];
+    await admin.firestore().collection('teams').where('leadUser', "==", userid).get()
+        .then(snapshot => {
+            if (!snapshot.empty) { // If he/she is, get members of the team
+                teamMemmbers = snapshot.docs[0].data().members;
+            } else {
+                return null;
+            }
+
+        }).catch(err => console.log(err))
+    if (teamMemmbers.length > 0) {  // If the team has at least one memeber, retrieve their request data
+        await Promise.all(teamMemmbers.map(async (member) => {
+            await admin.firestore().collection('leaveRequests').where("createdBy", "==", member).get().
+                then(querySnapshot => {
+                    querySnapshot.docs.map(doc => {
+                        const docObject = doc.data();
+                        docObject.id = doc.id;
+                        leaveRequestArray.push(docObject);
+
+                    })
+                }).catch(err => { return null; });
+        }))
+    }
+    return leaveRequestArray;
+}
+const _getAdminLeaves = async(userid) => {
+    const leaveRequestArray = [];
+    await admin.firestore().collection('leaveRequests').get().then(querySnapshot => {
+        querySnapshot.docs.map(doc => {
+            const docObject = doc.data();
+            docObject.id = doc.id;
+            leaveRequestArray.push(docObject);
+        })
+    }).catch(err => { return null; });
+    return leaveRequestArray;
+}
 const _getUser = (userid) => {
     return new Promise((resolve, reject) => {
-        admin.firestore().doc(`users/${userid}`).get().then(response => {           
-            resolve(response.data());       
+        admin.firestore().doc(`users/${userid}`).get().then(response => {
+            resolve(response.data());
         }).catch(error => {
             reject(error);
-        });   
-    });     
+        });
+    });
 }
 
 const _isAdmin = (user) => {
     return user.isAdmin === true;
-} 
+}
 
 const _isApprover = (user) => {
     return user.isApprover === true;
 }
 
-exports.Test = functions.https.onCall(async (data,context) => {
+exports.Test = functions.https.onCall(async (data, context) => {
     let user;
     const id = context.auth.uid;
     await _getUser(id).then(async response => {
